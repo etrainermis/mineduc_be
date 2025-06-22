89b13694f287678e36cdd95f78ccbc16a543879d
Yvette_Manishimwe <yvettemanishimwe9@gmail.com>
2025-06-22 13:11:04 +0200

    editing delete delegate api

--- src/common/enums/EEventType.enum.ts
+++ src/common/enums/EEventType.enum.ts
@@ -1,6 +1,6 @@
 
 export enum EEventType {
-    GLOBAL_SKILL_CONNECT = '4th EAC World Kiswahili Language Day Celebrations',
+    GLOBAL_SKILL_CONNECT = 'Global Skill Connect',
     TVET_EXPO = 'TVET EXPO',
     INTERMINISTERIAL_SUMMIT = 'Interministerial Summit',
   }
